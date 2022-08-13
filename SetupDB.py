a7524cc3f1fe2ba153ffa8ae012d6d37a5ef8e70
Bhagya <bhagya02.p@gmail.com>
2022-08-12 19:28:09 -0600

    login page complete, sign up started

--- SetupDB.py
+++ SetupDB.py
@@ -1,6 +1,15 @@
 from pymongo import MongoClient
-import json
 
 def Create_indexes(db):
     db["Users"].create_index([("Email","hashed")])
     db["Users"].create_index([("Phone","hashed")])
+
+def main():
+    Collection_Names = ["Users"]
+    Port = ""
+    Client = MongoClient(Port)
+    Db = Client["DB"]
+    Create_indexes()
+
+if __name__ == "__main__":
+    main()
