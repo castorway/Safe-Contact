34245540477124c127b5a90d0b8a6b4a2c854b3e
Bhagya <bhagya02.p@gmail.com>
2022-08-13 14:52:31 -0600

    jinja added

--- client/routes.py
+++ client/routes.py
@@ -1,5 +1,8 @@
+from sqlite3 import connect
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from flask_login import login_required, current_user
+
+from .models import User, Connection, user_connection_table
 from . import models
 from . import db
 
@@ -8,7 +11,10 @@ routes = Blueprint('routes', __name__)
 @routes.route('/', methods=['GET', 'POST'])
 # @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    users = User.query.all()
+    connects = Connection.query.all()
+
+    return render_template('home.html', user=current_user, users=users, connects=connects)
 
 @routes.route('/view', methods=['GET', 'POST'])
 def view():
