072ac91b699f442c90b160674e3e1b4aa6e79eeb
Bhagya <bhagya02.p@gmail.com>
2022-08-13 16:25:21 -0600

    forward messaging

--- client/sms_scheduler.py
+++ client/sms_scheduler.py
@@ -15,6 +15,8 @@ DEBUG = True
 load_dotenv()
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
+
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 def send_texts(scheduler):
@@ -29,28 +31,33 @@ def send_texts(scheduler):
         now = datetime.now()
 
         this_time = now.time()
-        earlier_time = (now - timedelta(minutes=TOLERANCE)).time()
-        print(this_time, earlier_time)
 
-        connections = Connection.query.filter(Connection.start_time <= this_time)
-        connections = connections.filter(Connection.start_time >= earlier_time)
-
-        connections = connections.filter(Connection.end_time >= this_time)
+        connections = Connection.query.filter(Connection.start_time <= now)
+        connections = connections.filter(Connection.end_time >= now)
 
         for conn in connections:
-            if DEBUG: print(">", conn, conn.last_text, conn.interval, this_time, flush=True)
+            if DEBUG: print("> checking connection", conn.id, conn.last_text, conn.interval, this_time, flush=True)
 
-            if conn.last_text + conn.interval < this_time:
-                if DEBUG: print('>> havent texted yet; texting', conn, flush=True)
+            if conn.last_text + timedelta(minutes=conn.interval) < now:
+                if DEBUG: print('>> havent texted yet; texting', conn.id, flush=True)
+
+                # get contact user
+                contact = User.query.filter_by(id=conn.contact_id).first()
                 
                 # get user's phone number
-                country_code = conn.user.country_code
-                phone_number = conn.user.phone_number
+                country_code = contact.country_code
+                phone_number = contact.phone_number
                 number = country_code + phone_number
 
-                client.messages.create(to=number, from_="+15878585813", body=conn.text_contents)
+                if conn.text_contents == "" or conn.text_contents == None:
+                    text_contents = "This is a message from Safe Contact."
+                else:
+                    text_contents = conn.text_contents
+
+                print('contents:', text_contents)
+                client.messages.create(to=TWILIO_NUMBER, from_="+19788785139", body=text_contents)
                 
                 # set last time to now
-                conn.last_text = this_time
+                conn.last_text = now
 
         db.session.commit()
